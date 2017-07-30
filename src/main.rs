11c22fcb6dbf32def48b264d73c2bd2567c05d24
modeverv <modeverv@gmail.com>
2017-07-31 00:57:06 +0900

    bool and char

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,36 @@
-
-
-
 fn main() {
     println!("Hello, world!");
     let x = 5;
     print_x(x);
     let y = add_one(x);
-    println!("add_one:{}",y);
+    println!("add_one:{}", y);
     let f: fn(i32) -> i32 = add_one;
-    println!("f():{}",f(4));
+    println!("f():{}", f(4));
+    check_bool();
+    check_char();
 }
 
-
-fn print_x(x: i32){
-    println!("x is: {}",x);
+// 日本語コメント
+fn print_x(x: i32) {
+    println!("x is: {}", x);
 }
 
-fn add_one(x:i32) -> i32 {
+fn add_one(x: i32) -> i32 {
     x + 1
 }
 
+fn check_bool() {
+    let x = true;
+    //let y: bool = false;
+    if x {
+        println!("true!!");
+    }
+}
 
-
+fn check_char(){
+    let x = '💕';
+    println!("{}",x);
+}
 
 
 
