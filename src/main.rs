e083c050313526005fb38d95551537fc5e0e5c91
modeverv <modeverv@gmail.com>
2017-07-31 00:49:07 +0900

    fn2

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ fn main() {
     println!("Hello, world!");
     let x = 5;
     print_x(x);
+    let y = add_one(x);
+    println!("add_one:{}",y);
 }
 
 
@@ -12,7 +14,9 @@ fn print_x(x: i32){
     println!("x is: {}",x);
 }
 
-
+fn add_one(x:i32) -> i32 {
+    x + 1
+}
 
 
 
