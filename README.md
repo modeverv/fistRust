f01382d2f8e6485c405cf92306b5ea7ae6796fef
modeverv <modeverv@gmail.com>
2017-07-31 00:41:36 +0900

    add README.md fn

---
+++ README.md
@@ -0,0 +1,9 @@
+# My First Rust
+
+## version 
+1.6
+
+## date
+2017/07/30-
+
+## enjoy!!!
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+
+
+
 fn main() {
     println!("Hello, world!");
+    let x = 5;
+    print_x(x);
 }
+
+
+fn print_x(x: i32){
+    println!("x is: {}",x);
+}
+
+
+
+
+
+
+
+
+
+
+
+
